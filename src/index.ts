2c136aca070b14591fcc5017ba571c6c31e2e69d
MartonG11 <gabormarton91@gmailcom>
2023-08-10 18:09:27 +0200

    feat: public files added and served in app

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import * as express from "express";
+import * as path from "path";
 import { PORT } from "./config";
 
 const app = express();
+
+// Serve static files from the public directory
+app.use(express.static("public"));
+
+app.get("*", (req, res) => {
+  res.sendFile("index.html", { root: path.join(__dirname, "../public/") });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
